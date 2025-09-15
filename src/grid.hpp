66a759c3319921e5069376e76a1064db9894704c
Shivendra <shivendrapratapsrivastava69@gmail.com>
2025-09-15 16:28:57 +0530

    Create grid.hpp

---
+++ src/grid.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+class Grid {
+    public:
+        Grid(int width, int height, int cellSize)
+        : rows(height/cellSize), columns(width/cellSize), cellSize(cellSize), cells(rows, vector<int>(columns, 0)) {};
+        void Draw();
+        void SetValue(int row, int column, int value);
+        int GetValue(int row, int column);
+        bool IsWithinBounds(int row, int column);
+        int GetRows() { return rows; }
+        int GetColumns() { return columns; }
+        void FillRandom();
+        void Clear();
+        void ToggleCell(int row, int column);
+    private:
+        int rows;
+        int columns;
+        int cellSize;
+        vector<vector<int>> cells;
+};
